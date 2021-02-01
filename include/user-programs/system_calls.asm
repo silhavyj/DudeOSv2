e72ca22bcd483eab91d20049b49bd239171910e5
Jakub Silhavy <jakub.sil@seznam.cz>
2021-02-01 17:52:31 +0100

    added a bit of comments

--- include/user-programs/system_calls.asm
+++ include/user-programs/system_calls.asm
@@ -1,29 +1,59 @@
+;---------------------------------------
+; terminates the current process - exit
+; stack: [esp    ] return address
+;---------------------------------------
 [global _uexit]
 _uexit:
-    mov     eax, 100
-    int     0x80
-    ret
+    mov     eax, 100        ; 100 = system call number (exit)
+    int     0x80            ; call the interrupt (0x80 = system calls)
+    ret                     ; return
+;---------------------------------------
 
+;---------------------------------------
+; dynamic allocation for the current
+; process - malloc
+; pointer to the allocate piece of memory
+; in EAX register (return value)
+; stack: [esp + 4] number of bytes to allocate
+;        [esp    ] return address
+;---------------------------------------
 [global _umalloc]
 _umalloc:
-    mov     ebx, [esp + 4]
-    mov     eax, 103
-    int     0x80
-    ret
+    mov     ebx, [esp + 4]  ; ebx = number of bytes to be allocated
+    mov     eax, 103        ; 103 = system call number (malloc)
+    int     0x80            ; call the interrupt (0x80 = system calls)
+    ret                     ; return
+;---------------------------------------
 
+;---------------------------------------
+; frees the memory given by an address
+; as the parameter - free
+; stack: [esp + 4] address to the memory
+;        [esp    ] return address
+;---------------------------------------
 [global _ufree]
 _ufree:
-    mov     ebx, [esp + 4]
-    mov     eax, 104
-    int     0x80
-    ret
+    mov     ebx, [esp + 4]  ; ebx = address to the mem we want to free
+    mov     eax, 104        ; 104 = system call number (free)
+    int     0x80            ; call the interrupt (0x80 = system calls)
+    ret                     ; return
+;---------------------------------------
 
+;---------------------------------------
+; exectues the program given as a
+; parameter. The function returns PID of
+; the new process for further validation
+; (0 = error)
+; stack: [esp + 4] name of the program to be executed
+;        [esp    ] return address
+;---------------------------------------
 [global _uexec]
 _uexec:
-    mov     ebx, [esp + 4]
-    mov     eax, 105
-    int     0x80
-    ret
+    mov     ebx, [esp + 4]  ; ebx = name of the program to be exectued
+    mov     eax, 105        ; 105 = system call number (exec)
+    int     0x80            ; call the interrupt (0x80 = system calls)
+    ret                     ; return
+;---------------------------------------
 
 [global _ps]
 _ps:
