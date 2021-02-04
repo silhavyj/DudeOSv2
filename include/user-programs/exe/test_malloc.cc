f7e77fa5ebfc5b1aedcea3b1011f01a4173ffc50
Jakub Silhavy <jakub.sil@seznam.cz>
2021-02-04 10:38:41 +0100

    changed test_malloc.exe a bit

--- include/user-programs/exe/test_malloc.cc
+++ include/user-programs/exe/test_malloc.cc
@@ -1,15 +1,21 @@
 #include <system.h>
 
+void test();
+
 int main() {
     _uprintf("Test of array allocation!\n");
+    test();
+    test();
+    _uexit();
+}
 
+void test() {
     int i;
-    int *arr = (int *)_umalloc(10 * sizeof(int));
+    int *arr;
+    arr = (int *)_umalloc(10 * sizeof(int));
     for (i = 0; i < 10; i++) {
         arr[i] = i;
         _uprintf("arr[i]=%d &arr[i]=%x\n", arr[i], &arr[i]);
     }
     _ufree(arr);
-    
-    _uexit();
 }
